d56213f14fa47ae7bd5120e7ee07425e3e4a7bb3
JohnKouf99 <johnkouf1999@gmail.com>
2021-04-23 20:10:08 +0300

    Initial commit

---
+++ src/ChannelName.java
@@ -0,0 +1,9 @@
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+
+public class ChannelName {
+    String channelName;
+    List<String> hashtagsPublished = new ArrayList<String>();
+    HashMap usersVideoFilesMap = new HashMap<String,ArrayList<Value>>();
+}
---
+++ src/Value.java
@@ -0,0 +1,5 @@
+
+
+public class Value {
+    //VideoFile videoFile;
+}
